06cd0e53d89ae53ec337f67721369d368d296d11
choyoon88 <azureluster@gmail.com>
2023-07-24 20:40:30 +0000

    added function for the user's input to show up on each cone

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,29 @@ function spin() {
     const spinner = document.getElementById('spinner');
     // const options = spinner.getElementsByClassName('option');
     const randomAngle = Math.floor(Math.random() * 1800);
-    
+
     spinner.style.transition = 'transform 3s ease-out';
     spinner.style.transform = `rotate(${randomAngle}deg)`;
     console.log(`rotate(${randomAngle}deg)`);
 }
 
+const textInputs = document.querySelectorAll('#text-input input');
 
+textInputs.forEach((input, index) => {
+    input.oninput = function () {
+        updateConeText(index, input.value);
+    };
+});
+
+function updateConeText(index, value) {
+    // Get reference to the specific option and its text element
+    const option = spinner.querySelectorAll('.option')[index];
+    const textElement = option.querySelector('.text h5');
+
+    // Update the text content of the corresponding cone
+    if (textElement) {
+        textElement.textContent = value;
+    } else {
+        option.querySelector('.text').textContent = value;
+    }
+}
