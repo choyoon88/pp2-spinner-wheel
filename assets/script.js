b35afb98e7a97f04b7e80c5b8e4d2d5d51dbc3ec
choyoon88 <azureluster@gmail.com>
2023-07-27 15:05:36 +0000

    Fixed spin function to spin more

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 function spin() {
     const spinner = document.getElementById('spinner');
-    // const options = spinner.getElementsByClassName('option');
-    const randomAngle = Math.floor(Math.random() * 1800);
+
+    const minValue = 1080;
+    const maxValue = 9999;
+    const randomAngle = Math.floor(Math.random() * (maxValue - minValue) + minValue);
 
     spinner.style.transition = 'transform 3s ease-out';
     spinner.style.transform = `rotate(${randomAngle}deg)`;
-    console.log(`rotate(${randomAngle}deg)`);
+    console.log(`rotate(${randomAngle}deg)`)
+}
+
+function resetSpinner() {
+    const spinner = document.getElementById('spinner');
+    spinner.style.transform = 'rotate(0deg)'; 
 }
 
 const textInputs = document.querySelectorAll('#text-input input');
